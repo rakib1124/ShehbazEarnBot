75172e6344745d173fae6696ed43975aeb6e0016
rakib1124 <141074580+rakib1124@users.noreply.github.com>
2026-07-02 23:43:04 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+BOT_TOKEN = 8881368619:AAHgkjE53t53v7PZE4EULEJiPHdqFSzPhHU
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("👋 Welcome to Shehbaz Earn Bot!")
+
+app = Application.builder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
